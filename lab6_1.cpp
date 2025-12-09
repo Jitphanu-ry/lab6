6a80197546449cfd7b918892f78bfce18c268198
Jitphanu-ry <jitphanuriyasarn@gmail.com>
2025-12-09 16:28:59 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main(){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int number,odd=0,even=0;
+    cin >> number;
+    while(number != 0){
+        if(number%2 == 0){
+            even++;
+        }else{
+            odd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> number;
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
